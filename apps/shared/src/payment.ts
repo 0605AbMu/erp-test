cf76ad4d21dbf0d961cc6dcd36d6cd86cd373900
0605AbMu <0605AbMu@gmail.com>
2026-09-12 09:21:06 +0500

    feat(payment): init payment module

---
+++ apps/shared/src/payment.ts
@@ -0,0 +1,17 @@
+export enum Currency {
+    UZS = "UZS",
+    USD = "USD",
+    RUB = "RUB"
+}
+
+export enum PaymentStatus {
+    PENDING = "pending",
+    PAID = "paid",
+    FAILED = "failed",
+    REFUNDED = "refunded"
+}
+
+export enum PaymentMethod {
+    CASH = 'cash',
+    CARD = 'card'
+}
