4076fbccfea7bf30b39bc666e2bc782c24d44fb9
0605AbMu <0605AbMu@gmail.com>
2026-09-12 15:26:05 +0500

    feature(web): add menu items

--- apps/web/src/components/common/layouts/dashboard.layout.tsx
+++ apps/web/src/components/common/layouts/dashboard.layout.tsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
 import {
-    DesktopOutlined,
-    FileOutlined,
+    BankFilled,
     PieChartOutlined,
-    TeamOutlined,
-    UserOutlined,
+    UserOutlined
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
-import { Outlet } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -29,15 +27,9 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
-        getItem('Tom', '3'),
-        getItem('Bill', '4'),
-        getItem('Alex', '5'),
-    ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
+    getItem('Users', '/dashboard/users', <UserOutlined />),
+    getItem('Payments', '/dashboard/payments', <BankFilled />),
+    getItem('Reports', '/dashboard/reports', <PieChartOutlined />)
 ];
 
 export const DashboardLayout = () => {
@@ -45,20 +37,21 @@ export const DashboardLayout = () => {
     const {
         token: { colorBgContainer, borderRadiusLG },
     } = theme.useToken();
+    const navigate = useNavigate();
 
     const currentYear = new Date().getFullYear();
 
     return (
-        <Layout style={{ minHeight: '100vh' }}>
+        <Layout style={{ minHeight: '100vh', width: '100vw' }}>
             <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
                 {/* <div>sadasdsd</div> */}
-                <div className="demo-logo-vertical" />
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+                <div className="demo-logo-vertical" style={{ height: '60px' }} />
+                <Menu theme="dark" defaultSelectedKeys={['/users']} mode="inline" items={items} onClick={({ key }) => navigate(key)} />
             </Sider>
             <Layout>
                 <Header style={{ padding: 0, background: colorBgContainer }} />
                 <Content style={{ margin: '0 16px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }} items={[{ title: 'User' }, { title: 'Bill' }]} />
+                    <Breadcrumb style={{ margin: '16px 0' }}  />
                     <div
                         style={{
                             padding: 24,
@@ -71,7 +64,7 @@ export const DashboardLayout = () => {
                     </div>
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>
-                    Ant Design ©{currentYear} Created by Ant UED
+                    Test ERP©{currentYear}
                 </Footer>
             </Layout>
         </Layout>
