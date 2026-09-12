4d5d08059d7fa82f952977ae717c2cbd279d4705
0605AbMu <0605AbMu@gmail.com>
2026-09-12 08:53:59 +0500

    feat: add auto-convert bigint to number

--- apps/api/src/db/db.module.ts
+++ apps/api/src/db/db.module.ts
@@ -1,5 +1,8 @@
 import { Global, Module } from '@nestjs/common';
 import { DbService } from './db.service.js';
+import { types } from 'pg';
+
+types.setTypeParser(20, (value) => Number(value));
 
 @Global()
 @Module({
