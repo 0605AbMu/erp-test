39e4c296145eb34fc1d75e68ffbda00242a0e20b
0605AbMu <0605AbMu@gmail.com>
2026-09-13 11:06:56 +0500

    refactor(shared): group types

--- apps/shared/src/index.ts
+++ apps/shared/src/index.ts
@@ -2,6 +2,6 @@ export * from "./roles.js";
 export * from "./payment.js";
 export * from "./report.js";
 export * from "./api-response.js";
-export * from "./users.types.js";
-export * from "./auth.types.js";
+export * from "./types/users.types.js";
+export * from "./types/auth.types.js";
 export * from "./query.interface.js";
